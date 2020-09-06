787599169e6da734301cf61da182e91c2a53b9eb
ThibautChamoux <thibaut.chamoux@gmail.com>
2020-09-06 18:10:10 +0200

    notification service -> add notifyUser mutation + resolver

---
+++ services/users/src/functions/notifications/resolvers/notifyUser.js
@@ -0,0 +1,36 @@
+const {
+  graphql: { formatSuccess },
+} = require('claptime-commons/api-responses');
+const { getParam } = require('claptime-commons/ssm');
+const { appSyncClient, gql } = require('claptime-commons/appsync');
+
+const createNotification = async (userId, type, payload) => {
+  console.log(`Create WEB notification for user ${userId}`);
+  console.log(`Type : ${type}`);
+  console.log(`Payload : ${JSON.stringify(payload)}`);
+  await appSyncClient.mutate({
+    mutation: gql(`mutation CreateNotification($input: CreateNotificationInput!) {
+      createNotification(input: $input) {
+        id
+      }
+    }`),
+    variables: {
+      input: {
+        userId,
+        type,
+        payload: JSON.stringify(payload),
+      },
+    },
+  });
+};
+
+module.exports = async ({
+  arguments: { userId, type, channels, payload },
+  identity,
+}) => {
+  let response;
+  if (channels.includes('WEB')) {
+    await createNotification(userId, type, payload);
+  }
+  return formatSuccess(response);
+};
