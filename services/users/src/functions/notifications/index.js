f52c2842500e27f1a2605fe7c608f8d875744bf6
ThibautChamoux <thibaut.chamoux@gmail.com>
2020-09-06 18:10:10 +0200

    notification service -> add notifyUser mutation + resolver

--- services/users/src/functions/notifications/index.js
+++ services/users/src/functions/notifications/index.js
@@ -1,11 +1,15 @@
 const { getHandler } = require('claptime-commons/appsync');
 
 const setNotificationPreference = require('./resolvers/setNotificationPreference');
+const notifyUser = require('./resolvers/notifyUser');
 
 module.exports.handler = getHandler({
   Mutation: {
     setNotificationPreference: {
       handler: setNotificationPreference,
+    },
+    notifyUser: {
+      handler: notifyUser,
     },
   },
 });
