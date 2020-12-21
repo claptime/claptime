8e4fd41bceaff830b92a331f5b711ff9e7fe894f
ThibautChamoux <thibaut.chamoux@gmail.com>
2020-12-21 22:14:47 +0100

    lint

--- web/src/components/organisms/LandingOfflineSection/index.jsx
+++ web/src/components/organisms/LandingOfflineSection/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 import consts from 'claptime/consts';
@@ -38,12 +38,10 @@ const BackgroundContainer = styled.div`
 `;
 
 const OfflineSection = () => {
-  window.addEventListener(
-    'scroll',
-    () =>
-      (document.getElementById('claptime-background-container').style.opacity =
-        1 - window.scrollY / window.innerHeight),
-  );
+  window.addEventListener('scroll', () => {
+    document.getElementById('claptime-background-container').style.opacity =
+      1 - window.scrollY / window.innerHeight;
+  });
 
   return (
     <>
