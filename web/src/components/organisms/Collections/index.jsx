9920c54b8b188a7b39006de74f65d6a892b32807
ThibautChamoux <thibaut.chamoux@gmail.com>
2020-12-21 21:58:01 +0100

    rework on Collections display (use grid)

--- web/src/components/organisms/Collections/index.jsx
+++ web/src/components/organisms/Collections/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Typography } from 'antd';
+import { Typography, List } from 'antd';
 
 import { ContactUsButton } from 'claptime/components/atoms';
 import { Cards } from 'claptime/components/molecules';
@@ -28,11 +28,25 @@ const Collections = () => {
 
   return (
     <div>
-      {items.map((collection) => (
-        <div style={{ margin: '36px 0' }} key={collection.id}>
-          <Cards.Collection collection={collection} />
-        </div>
-      ))}
+      <List
+        grid={{
+          gutter: 48,
+          xs: 1,
+          sm: 1,
+          md: 1,
+          lg: 1,
+          xl: 1,
+          xxl: 2,
+        }}
+        dataSource={items}
+        renderItem={(collection) => (
+          <List.Item>
+            <div style={{ marginTop: '18px' }}>
+              <Cards.Collection collection={collection} />
+            </div>
+          </List.Item>
+        )}
+      />
       <div
         style={{ display: 'flex', alignItems: 'baseline', flexWrap: 'wrap' }}
       >
