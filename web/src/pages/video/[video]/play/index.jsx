b00fe92d95efd8518344e5fdb8e9aa1ca750d999
Christophe Bougère <christophe.bougere@gmail.com>
2021-02-07 16:43:37 +0100

    conflict fix

--- web/src/pages/video/[video]/play/index.jsx
+++ web/src/pages/video/[video]/play/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import dynamic from 'next/dynamic';
 import Router from 'next/router';
 import { useTranslation } from 'react-i18next';
